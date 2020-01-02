8f1243008c5b14027c3ffaae2e57c533bdfa3ccd
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-01-01 23:50:03 -0600

    feat: Add the FontFactory header

---
+++ inc/graphics/FontFactory.h
@@ -0,0 +1,71 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: FontFactory.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2019 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef FONTFACTORY_H_
+#define FONTFACTORY_H_
+
+#include <memory>
+#include <vector>
+
+#include <boost/shared_ptr.hpp>
+
+#include "graphics/FontStyle.h"
+#include "graphics/FontWeight.h"
+
+class Font;
+class FontFactoryPrivate;
+
+class FontFactory
+{
+  public:
+
+    FontFactory();
+
+    ~FontFactory();
+
+    static std::auto_ptr<Font> createFont(const std::string& typeface,
+                                          FontWeight::Value fontWeight,
+                                          FontStyle::Value fontStyle,
+                                          std::size_t fontPointSize);
+
+    static std::auto_ptr<Font> createFont(const std::string& typeface,
+                                          FontWeight::Value fontWeight,
+                                          std::size_t fontPointSize);
+
+    static std::auto_ptr<Font> createFont(const std::string& typeface,
+                                          FontStyle::Value fontStyle,
+                                          std::size_t fontPointSize);
+
+    static std::auto_ptr<Font> createFont(const std::string& typeface,
+                                          std::size_t fontPointSize);
+
+    boost::shared_ptr<Font> createSharedFont(const std::string& typeface,
+                                             FontWeight::Value fontWeight,
+                                             FontStyle::Value fontStyle,
+                                             std::size_t fontPointSize);
+
+    boost::shared_ptr<Font> createSharedFont(const std::string& typeface,
+                                             FontWeight::Value fontWeight,
+                                             std::size_t fontPointSize);
+
+    boost::shared_ptr<Font> createSharedFont(const std::string& typeface,
+                                             FontStyle::Value fontStyle,
+                                             std::size_t fontPointSize);
+
+    boost::shared_ptr<Font> createSharedFont(const std::string& typeface,
+                                             std::size_t fontPointSize);
+
+  private:
+
+    std::auto_ptr<FontFactoryPrivate> d;
+};
+#endif /* FONTFACTORY_H_ */
