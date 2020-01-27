0ac1ea1c2ee3330186762ed93faee6eb967a4ac8
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-01-26 23:16:43 -0600

    feat: Add the IDrawable interface

---
+++ inc/graphics/IDrawable.h
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: IDrawable.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GRAPHICS_IDRAWABLE_H_
+#define GRAPHICS_IDRAWABLE_H_
+
+class IDrawable
+{
+  protected:
+
+    IDrawable()
+    {
+
+    }
+
+  public:
+
+    virtual ~IDrawable()
+    {
+
+    }
+};
+#endif /* GRAPHICS_IDRAWABLE_H_ */
