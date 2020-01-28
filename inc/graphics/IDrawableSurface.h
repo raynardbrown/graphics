481fa472e1ec5f9d9953752b411c0002263c39c8
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-01-27 22:59:58 -0600

    feat: Add the IDrawableSurface interface

---
+++ inc/graphics/IDrawableSurface.h
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: IDrawableSurface.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GRAPHICS_IDRAWABLESURFACE_H_
+#define GRAPHICS_IDRAWABLESURFACE_H_
+
+class IDrawableSurface
+{
+  protected:
+
+    IDrawableSurface()
+    {
+
+    }
+
+  public:
+
+    virtual ~IDrawableSurface()
+    {
+
+    }
+
+    virtual int getWidth() const = 0;
+    virtual int getHeight() const = 0;
+};
+#endif /* GRAPHICS_IDRAWABLESURFACE_H_ */
