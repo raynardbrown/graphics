b14c2556c15c9ee193a17887cf43fc802ebcc887
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-02 23:53:15 -0600

    feat: Add the DrawableSurfaceContext header

---
+++ src/graphics/private/windows/DrawableSurfaceContext.h
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: DrawableSurfaceContext.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GRAPHICS_PRIVATE_WINDOWS_DRAWABLESURFACECONTEXT_H_
+#define GRAPHICS_PRIVATE_WINDOWS_DRAWABLESURFACECONTEXT_H_
+
+#include <windows.h>
+
+struct DrawableSurfaceContext
+{
+  public:
+
+    HWND hwnd;
+};
+#endif /* GRAPHICS_PRIVATE_WINDOWS_DRAWABLESURFACECONTEXT_H_ */
