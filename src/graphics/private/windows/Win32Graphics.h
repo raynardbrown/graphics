bb7d0b35bb9c6dcd92f112382fa1b582eb7484a6
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-03 23:28:07 -0600

    feat: Add the Win32Graphics header

---
+++ src/graphics/private/windows/Win32Graphics.h
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: Win32Graphics.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GRAPHICS_PRIVATE_WINDOWS_WIN32GRAPHICS_H_
+#define GRAPHICS_PRIVATE_WINDOWS_WIN32GRAPHICS_H_
+
+#include <memory>
+#include <windows.h>
+
+#include "graphics/Graphics.h"
+
+class IDrawable;
+class Win32GraphicsPrivate;
+
+class Win32Graphics : public Graphics
+{
+  public:
+
+    Win32Graphics(IDrawable * drawable);
+
+    Win32Graphics(IDrawable * drawable, HDC hdc);
+
+    virtual ~Win32Graphics();
+
+    virtual void setBackgroundColor(const Color * backgroundColor);
+
+    /**
+     * Set the color that this Graphics will use to draw text.
+     *
+     * @param[in] textColor the new color that this Graphics will use to draw
+     * text.
+     */
+    virtual void setTextColor(const Color * textColor);
+
+  private:
+
+    std::auto_ptr<Win32GraphicsPrivate> d;
+};
+#endif /* GRAPHICS_PRIVATE_WINDOWS_WIN32GRAPHICS_H_ */
