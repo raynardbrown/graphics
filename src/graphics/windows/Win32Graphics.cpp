65c3ede6554d359f7327b9d44e542a6e02caa181
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-05 23:40:26 -0600

    feat: Add the Win32Graphics source

---
+++ src/graphics/windows/Win32Graphics.cpp
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: Win32Graphics.cpp
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "graphics/private/windows/Win32GraphicsPrivate.h"
+
+#include "graphics/private/windows/Win32Graphics.h"
+
+Win32GraphicsPrivate::Win32GraphicsPrivate(IDrawable * drawable)
+:drawable(drawable),
+ hdc(nullptr),
+ memoryDC(nullptr),
+ compatibleBitmap(nullptr),
+ oldBitmap(nullptr),
+ doubleBufferingEnabled(true),
+ clientHandle(false)
+{
+}
+
+Win32GraphicsPrivate::Win32GraphicsPrivate(IDrawable * drawable,
+                                           HDC hdc)
+:drawable(drawable),
+ hdc(hdc),
+ memoryDC(nullptr),
+ compatibleBitmap(nullptr),
+ oldBitmap(nullptr),
+ doubleBufferingEnabled(false), // don't use the memory dc the client is supply an HDC
+ clientHandle(true)
+{
+}
