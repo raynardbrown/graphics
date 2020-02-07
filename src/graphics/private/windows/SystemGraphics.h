d5e8f84c7651b56207c433d72b4e77bce1a99389
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-06 23:05:06 -0600

    feat: Add the SystemGraphics header

---
+++ src/graphics/private/windows/SystemGraphics.h
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: SystemGraphics.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GRAPHICS_PRIVATE_WINDOWS_SYSTEMGRAPHICS_H_
+#define GRAPHICS_PRIVATE_WINDOWS_SYSTEMGRAPHICS_H_
+
+#include <windows.h>
+
+class IDrawable;
+
+class SystemGraphics
+{
+  public:
+
+    SystemGraphics(IDrawable * drawable);
+
+    SystemGraphics(IDrawable * drawable, HDC hdc);
+
+    virtual ~SystemGraphics();
+};
+#endif /* GRAPHICS_PRIVATE_WINDOWS_SYSTEMGRAPHICS_H_ */
