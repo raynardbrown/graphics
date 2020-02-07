7cd5ea706e71447621df60f5121dcec2e705159e
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-06 23:01:17 -0600

    feat: Add the ScreenGraphics header

---
+++ src/graphics/private/windows/ScreenGraphics.h
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: ScreenGraphics.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GRAPHICS_PRIVATE_WINDOWS_SCREENGRAPHICS_H_
+#define GRAPHICS_PRIVATE_WINDOWS_SCREENGRAPHICS_H_
+
+class ScreenGraphics
+{
+  public:
+
+    ScreenGraphics();
+
+    virtual ~ScreenGraphics();
+};
+#endif /* GRAPHICS_PRIVATE_WINDOWS_SCREENGRAPHICS_H_ */
