3f857b783029b0e2aea3bb00fb35da85988d1820
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-06 22:42:23 -0600

    feat: Add the GraphicsFactory header

---
+++ inc/graphics/GraphicsFactory.h
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: GraphicsFactory.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GRAPHICS_GRAPHICSFACTORY_H_
+#define GRAPHICS_GRAPHICSFACTORY_H_
+
+class GraphicsFactory
+{
+  private:
+
+    GraphicsFactory()
+    {
+
+    }
+
+  public:
+
+    ~GraphicsFactory()
+    {
+
+    }
+};
+#endif /* GRAPHICS_GRAPHICSFACTORY_H_ */
