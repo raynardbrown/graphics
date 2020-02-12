4acf6d6a77517fe6fb79c5a1832f33e39f281eab
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-11 23:55:15 -0600

    feat: Sub-class Win32Graphics

--- src/graphics/private/windows/ScreenGraphics.h
+++ src/graphics/private/windows/ScreenGraphics.h
@@ -13,14 +13,20 @@
 #ifndef GRAPHICS_PRIVATE_WINDOWS_SCREENGRAPHICS_H_
 #define GRAPHICS_PRIVATE_WINDOWS_SCREENGRAPHICS_H_
 
-#include "graphics/Graphics.h"
+#include "graphics/private/windows/Win32Graphics.h"
 
-class ScreenGraphics : public Graphics
+class ScreenGraphicsPrivate;
+
+class ScreenGraphics : public Win32Graphics
 {
   public:
 
     ScreenGraphics();
 
     virtual ~ScreenGraphics();
+
+  protected:
+
+    ScreenGraphics(ScreenGraphicsPrivate& d);
 };
 #endif /* GRAPHICS_PRIVATE_WINDOWS_SCREENGRAPHICS_H_ */
