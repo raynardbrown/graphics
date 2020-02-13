155f45cd96aea3d43d39d9031a8c752b042eed72
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-13 05:59:13 -0600

    feat: Add the ISetBackgroundColorImpl interface

---
+++ src/graphics/private/ISetBackgroundColorImpl.h
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: ISetBackgroundColorImpl.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GRAPHICS_PRIVATE_ISETBACKGROUNDCOLORIMPL_H_
+#define GRAPHICS_PRIVATE_ISETBACKGROUNDCOLORIMPL_H_
+
+class Color;
+
+class ISetBackgroundColorImpl
+{
+  protected:
+
+    ISetBackgroundColorImpl()
+    {
+
+    }
+
+    virtual ~ISetBackgroundColorImpl()
+    {
+
+    }
+
+    virtual void setBackgroundColor(const Color * backgroundColor) = 0;
+};
+#endif /* GRAPHICS_PRIVATE_ISETBACKGROUNDCOLORIMPL_H_ */
