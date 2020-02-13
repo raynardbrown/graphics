5c1d770e77cbce43935248a396fb5508ad8bd86d
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-13 06:05:13 -0600

    feat: Add Win32GraphicsSetBackgroundColorImpl.cpp

---
+++ src/graphics/windows/Win32GraphicsBackgroundColorImpl.cpp
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: Win32GraphicsSetBackgroundColorImpl.cpp
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <windows.h>
+
+#include "graphics/Color.h"
+#include "graphics/IDrawable.h"
+#include "graphics/IDrawableSurface.h"
+
+#include "graphics/private/windows/Win32Graphics.h"
+#include "graphics/private/windows/Win32GraphicsPrivate.h"
+
+#include "graphics/private/windows/Win32GraphicsSetBackgroundColorImpl.h"
+
+Win32GraphicsSetBackgroundColorImpl::Win32GraphicsSetBackgroundColorImpl(Win32Graphics * win32Graphics)
+:ISetBackgroundColorImpl(),
+ win32Graphics(win32Graphics)
+{
+
+}
+
+Win32GraphicsSetBackgroundColorImpl::~Win32GraphicsSetBackgroundColorImpl()
+{
+
+}
+
+void Win32GraphicsSetBackgroundColorImpl::setBackgroundColor(const Color * backgroundColor)
+{
+  HDC hdc;
+
+  if(win32Graphics->dRoot->doubleBufferingEnabled)
+  {
+    // TODO: make sure the memory has been initialized
+    hdc = win32Graphics->dRoot->memoryDC;
+  }
+  else
+  {
+    hdc = win32Graphics->dRoot->hdc;
+  }
+
+  // TODO: Remember the default color of a bitmap is black.
+  //       Put in an API to let the user decide the color of the bitmap.
+
+  // TODO: For now do this. If not the entire background will be black when you
+  //       draw
+  RECT rc;
+
+  rc.left = 0;
+  rc.top = 0;
+  rc.right = win32Graphics->dRoot->drawable->getDrawableSurface()->getWidth();
+  rc.bottom = win32Graphics->dRoot->drawable->getDrawableSurface()->getHeight();
+
+  HBRUSH brush = ::CreateSolidBrush(backgroundColor->getColorValue());
+
+  ::FillRect(hdc, &rc, brush);
+
+  ::DeleteObject(brush);
+}
